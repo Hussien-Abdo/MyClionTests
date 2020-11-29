3576499d32a01fa42f8184cbabce230f1a336770
Hussien Abdo <habdo@rhrk.uni-kl.de>
2020-11-29 09:02:21 +0100

    initial commit

---
+++ cmake-build-debug/CMakeFiles/MyClionTests.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyClionTests.dir/main.cpp.obj"
+  "MyClionTests.exe"
+  "MyClionTests.exe.manifest"
+  "MyClionTests.lib"
+  "MyClionTests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyClionTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
